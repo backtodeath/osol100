229e3c8a0422c97177d0e2365ae5f5698af91c7c
llirola <lirolaluis@gmail.com>
2024-12-23 12:34:08 +0100

    add charts

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
@@ -14,7 +14,7 @@ import { interval } from 'rxjs';
 })
 export default class HomeComponent implements OnInit {
   coinApiUrl =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ai16z,fartcoin,grass,goatseus-maximus,io,act-i-the-ai-prophecy,zerebro,nosana,griffain,tars-protocol,ai-rig-complex,eliza,alchemist-ai,memes-ai,degen-spartan-ai,dasha,dolos-the-bully,kween,orbit-2,fxn,top-hat,shoggoth,agenttank,deep-worm,big-pharmai,bongo-cat,numogram,ava-ai,opus-2,obot,project89,chaos-and-disorder,meow,koala-ai,kitten-haimer,pippin,max-2,aimonica-brands,autonomous-virtual-beings,forest,solaris-ai,synesis-one,moe-4,universal-basic-compute,mizuki,naitzsche,slopfather,the-lokie-cabal,tensor,arok-vc,aiwithdaddyissues,bloomsperg-terminal,omega-2,thales-ai,keke-terminal,horny,quasar-2,ropirito,kolin,kwantxbt,agent-rogue,dither,duck-ai,centience,iq6900,darksun,weird-medieval-memes,yousim,sensus,ocada-ai,singularry,naval-ai,kira-2,kirakuru,brot,effective-accelerationism,cheshire-grin,limbo,size,neroboss,gmika,kira-3,convo,sqrfund,ugly-dog,gemxbt,roastmaster9000,nova-on-mars,sendor,flowerai,dojo-protocol,internosaur,devin,lea-ai,rex-3,aletheia,mona-arcane,apicoin,cyphomancer,lucy-ai&order=market_cap_desc&per_page=250&sparkline=false&locale=en';
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ai16z,fartcoin,grass,goatseus-maximus,io,act-i-the-ai-prophecy,zerebro,nosana,griffain,tars-protocol,ai-rig-complex,eliza,alchemist-ai,memes-ai,degen-spartan-ai,dasha,dolos-the-bully,kween,orbit-2,fxn,top-hat,shoggoth,agenttank,deep-worm,big-pharmai,bongo-cat,numogram,ava-ai,opus-2,obot,project89,chaos-and-disorder,meow,koala-ai,kitten-haimer,pippin,max-2,aimonica-brands,autonomous-virtual-beings,forest,solaris-ai,synesis-one,moe-4,universal-basic-compute,mizuki,naitzsche,slopfather,the-lokie-cabal,tensor,arok-vc,aiwithdaddyissues,bloomsperg-terminal,omega-2,thales-ai,keke-terminal,horny,quasar-2,ropirito,kolin,kwantxbt,agent-rogue,dither,duck-ai,centience,iq6900,darksun,weird-medieval-memes,yousim,sensus,ocada-ai,singularry,naval-ai,kira-2,kirakuru,brot,effective-accelerationism,cheshire-grin,limbo,size,neroboss,gmika,kira-3,convo,sqrfund,ugly-dog,gemxbt,roastmaster9000,nova-on-mars,sendor,flowerai,dojo-protocol,internosaur,devin,lea-ai,rex-3,aletheia,mona-arcane,apicoin,cyphomancer,lucy-ai&order=market_cap_desc&per_page=250&sparkline=true&locale=en';
   coins: any[] = []; // Holds the fetched coin data
   isLoading = true;
   refreshCooldown = 60;
@@ -27,10 +27,14 @@ export default class HomeComponent implements OnInit {
     this.startCooldownTimer();
   }
 
+  /* eslint-disable @typescript-eslint/no-unsafe-return */
   fetchCoins(): void {
     this.http.get<any[]>(this.coinApiUrl).subscribe({
       next: response => {
-        this.coins = response;
+        this.coins = response.map(coin => ({
+          ...coin,
+          sparkline_in_7d: coin.sparkline_in_7d || { price: [] },
+        }));
         this.isLoading = false;
       },
       error: error => {
